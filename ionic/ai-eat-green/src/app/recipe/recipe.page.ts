f8e74d4797aec781a4b7aa54eed048d0bb77d9f1
Karl Cui <karl.cui@mail.utoronto.ca>
2020-01-11 16:00:57 -0500

    Small fixes and toolbar color

--- ionic/ai-eat-green/src/app/recipe/recipe.page.ts
+++ ionic/ai-eat-green/src/app/recipe/recipe.page.ts
@@ -12,7 +12,7 @@ export class RecipePage implements OnInit {
   recipe_name = "Raw Zucchini Pesto Pasta"
   recipe_ingredients = ["2-3 medium zucchinis", "1/2 cup fresh basil", "1/4 cup raw pine nuts", "1/4 cup olive oil", "2 cloves raw garlic", "2 tablespoons nutritional yeast or grated pumpkin seeds"]
   recipe_preparation = [
-    "slice zucchini into fettuccine noodles with a vegetable peeler or mandolin. If you have a spiral vegetable cutter, it will make beautiful spaghetti noodles!",
+    "Slice zucchini into fettuccini noodles with a vegetable peeler or mandolin. If you have a spiral vegetable cutter, it will make beautiful spaghetti noodles!",
     "Blend all ingredients, except for the zucchini, in a food processor until smooth.",
     "Scoop zucchini pasta into bowls and top with pesto sauce.",
     "Add in vegetables of choice to add flavor, color and texture such as olives, sundried tomatoes, mushrooms, artichoke and colorful peppers."
