a54cec9f9bfb8287870f9daba71ab8de27c47c8e
Karl Cui <karl.cui@mail.utoronto.ca>
2020-01-14 20:22:36 -0500

    Referral

--- ionic/ai-eat-green/src/app/app.component.ts
+++ ionic/ai-eat-green/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { AlertController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -36,7 +36,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public alertController: AlertController,
   ) {
     this.initializeApp();
   }
@@ -46,5 +47,19 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  async presentAlertReferral() {
+    const alert = await this.alertController.create({
+      header: "Refer a friend",
+      subHeader: "Refer a friend and unlock rewards!",
+      message:  "<ion-icon name='link'></ion-icon> EASE810522",
+      buttons: [{
+          text: 'Done',
+          role: 'cancel',
+        }]
+    });
+
+    await alert.present();
   }
 }
