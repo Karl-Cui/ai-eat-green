4b862fe6ca2494e37af3c0cabfc47c453682e32e
3370sohail <3370sohail@gmail.com>
2020-02-01 16:24:47 -0500

    added in simple a to compare button

--- ionic/ai-eat-green/src/app/recipe/recipe.page.ts
+++ ionic/ai-eat-green/src/app/recipe/recipe.page.ts
@@ -10,7 +10,7 @@ export class RecipePage implements OnInit {
 
 
   recipe_name = "Zucchini Pesto Pasta"
-  recipe_ingredients = ["2-3 medium zucchinis", "1/2 cup fresh basil",  "500 mL Pesto sauce", "1/4 cup raw pine nuts", "1/4 cup olive oil", "2 cloves raw garlic", "2 tablespoons nutritional yeast or grated pumpkin seeds"]
+  recipe_ingredients = ["2-3 medium zucchinis", "1/2 cup fresh basil", "500 mL Pesto sauce", "1/4 cup raw pine nuts", "1/4 cup olive oil", "2 cloves raw garlic", "2 tablespoons nutritional yeast or grated pumpkin seeds"]
   recipe_preparation = [
     "Slice zucchini into fettuccini noodles with a vegetable peeler or mandolin. If you have a spiral vegetable cutter, it will make beautiful spaghetti noodles!",
     "Blend all ingredients, except for the zucchini, in a food processor until smooth.",
@@ -22,7 +22,7 @@ export class RecipePage implements OnInit {
     initialSlide: 1,
     speed: 200,
     slidesPerView: 2,
-    autoplay:true
+    autoplay: true
   };
 
   sustainability_figures = {
@@ -33,18 +33,35 @@ export class RecipePage implements OnInit {
 
   id: string;
 
-  constructor(private route: ActivatedRoute) {}
+  in_compare = false;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
     console.log(this.id)
   }
 
-  addtocompare(){
+  addtocompare() {
     let compareRecipes = JSON.parse(localStorage.getItem('compareRecipes'))
     compareRecipes.push(this.id)
     localStorage.setItem('compareRecipes', JSON.stringify(compareRecipes));
     console.log(localStorage.getItem('compareRecipes'))
+    this.in_compare = true;
+  }
+
+  removefromcompare() {
+    let current_id  = this.id
+    let compareRecipes = JSON.parse(localStorage.getItem('compareRecipes'))
+    let filtered = compareRecipes.filter(function (value, index, arr) {
+      if (value != current_id) {
+        return true
+      }
+      return false
+    });
+    localStorage.setItem('compareRecipes', JSON.stringify(filtered));
+    console.log(localStorage.getItem('compareRecipes'))
+    this.in_compare = false;
   }
 
 }
