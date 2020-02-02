cbc77917e8369a7142c047c83da54088282fb99a
Karl Cui <karl.cui@mail.utoronto.ca>
2020-02-01 19:16:51 -0500

    Challenge bar

--- ionic/ai-eat-green/src/app/challenge/challenge.page.ts
+++ ionic/ai-eat-green/src/app/challenge/challenge.page.ts
@@ -13,15 +13,21 @@ export class ChallengePage implements OnInit {
   challenges = [
     {
       'name': 'Post your Food Pictures',
-      'description': 'hello world'
+      'description': 'Post pictures of the recipe you made! This can be done on the recipe page.',
+      'image': '/assets/recipe/salad.jpg',
+      'joined': false
     },
     {
       'name': 'Meatless for a Week',
-      'description': 'hello world'
+      'description': 'Make one or more recipes per day for a week, but they must all be vegetarian recipes. It\'s time to go meatless!',
+      'image': '/assets/recipe/vegetable.jpg',
+      'joined': false
     },
     {
       'name': 'Try 3 new recipes',
-      'description': 'hello world'
+      'description': 'Cook 3 new recipes. Try searching up dishes or ingredients that you are interested in, or take a look at your recommended recipes!',
+      'image': '/assets/recipe/cooking.jpg',
+      'joined': false
     }
   ];
   id: string;
@@ -29,6 +35,10 @@ export class ChallengePage implements OnInit {
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
     console.log(this.id)
+  }
+
+  joinChallenge(id) {
+    this.challenges[id].joined = true;
   }
 
 }
